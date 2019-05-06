85a2f7e0d8f3abf2d66495af66342f114026557e
Gabriel <gabriel88944@hotmail.com>
2019-05-06 19:57:44 -0300

    Actualización de archivo .js

--- js/homebanking.js
+++ js/homebanking.js
@@ -1,7 +1,11 @@
 //Declaración de variables
 var nombreUsuario = "Gabriel Ríos";
-var saldoCuenta = 10000;
+var saldoCuenta = 1000;
 var limiteExtraccion = 3000;
+var servicioAgua = 350;
+var servicioTelefono = 425;
+var servicioLuz = 210;
+var servicioInternet = 570;
 
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
@@ -11,6 +15,7 @@ window.onload = function() {
     actualizarLimiteEnPantalla();
 }
 
+//
 function agregarDinero(dinero) {
   saldoCuenta += dinero;
   return saldoCuenta;
@@ -23,11 +28,28 @@ function restarDinero(dinero) {
 
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
+  var nuevoLimite = prompt("Ingrese nuevo límite de extracción:");
+  limiteExtraccion = parseInt(nuevoLimite);
 
+  actualizarLimiteEnPantalla();
+
+  alert("El nuevo límite de extracción es: " + limiteExtraccion);
 }
 
 function extraerDinero() {
 
+  var saldoAnterior = saldoCuenta;
+  var dinero = prompt("Ingrese dinero a extraer:");
+  var dineroAExtraer = parseInt(dinero);
+
+  restarDinero(dineroAExtraer);
+
+  alert("Has extraído: $" + dineroAExtraer +
+  ".\nSaldo anterior: $" + saldoAnterior +
+  ".\nSaldo actual: $" + saldoCuenta + ".");
+
+
+  actualizarSaldoEnPantalla();
 }
 
 function depositarDinero() {
@@ -47,7 +69,74 @@ function depositarDinero() {
 
 function pagarServicio() {
 
+  var servicios = prompt ("Ingrese el número que corresponda con el servicio que querés pagar.\n1 - Agua\n2 - Luz\n3 - Internet\n4 - Teléfono");
+
+  pagoServicios = parseInt(servicios);
+
+  saldoAnterior = saldoCuenta;
+
+  switch (pagoServicios) {
+    case 1:
+      pagarAgua();
+    break;
+    case 2:
+      pagarLuz();
+    break;
+    case 3:
+      pagarInternet();
+    break;
+    case 4:
+      pagarTelefono();
+    break;
+    default:
+    alert("No existe el servicio que seleccionaste");
+  }
+
+  actualizarSaldoEnPantalla();
+
+  function pagarAgua(){
+    if(saldoCuenta < servicioAgua){
+      alert("No hay suficiente saldo en tu cuenta para pagar este servicio.");
+    }else{
+      saldoCuenta -= servicioAgua;
+      alert("Has pagado el servicio de agua.\nSaldo anterior: $" + saldoAnterior +
+      ".\nDinero descontado: $" + servicioAgua + ".\nSaldo actual: $" + saldoCuenta + ".");
+    }
+  }
+
+  function pagarLuz(){
+    if(saldoCuenta < servicioLuz){
+      alert("No hay suficiente saldo en tu cuenta para pagar este servicio.");
+    }else{
+      saldoCuenta -= servicioLuz;
+      alert("Has pagado el servicio de luz.\nSaldo anterior: $" + saldoAnterior +
+      ".\nDinero descontado: $" + servicioLuz + ".\nSaldo actual: $" + saldoCuenta + ".");
+    }
+  }
+
+  function pagarInternet(){
+    if(saldoCuenta < servicioInternet){
+      alert("No hay suficiente saldo en tu cuenta para pagar este servicio.");
+    }else{
+      saldoCuenta -= servicioInternet;
+      alert("Has pagado el servicio de Internet.\nSaldo anterior: $" + saldoAnterior +
+      ".\nDinero descontado: $" + servicioInternet + ".\nSaldo actual: $" + saldoCuenta + ".");
+    }
+  }
+
+  function pagarTelefono(){
+    if(saldoCuenta < servicioTelefono){
+      alert("No hay suficiente saldo en tu cuenta para pagar este servicio.");
+    }else{
+      saldoCuenta -= servicioTelefono;
+      alert("Has pagado el servicio de teléfono.\nSaldo anterior: $" + saldoAnterior +
+      ".\nDinero descontado: $" + servicioTelefono + ".\nSaldo actual: $" + saldoCuenta + ".");
+    }
+  }
+
 }
+
+
 
 function transferirDinero() {
 
